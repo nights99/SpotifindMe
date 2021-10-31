b16ca5083070b65faa67fff82451be77030fe0a5
nights99 <jn@pynights.me.uk>
2021-10-31 12:30:44 +0000

    Working notification exit button

--- app/src/main/java/fi/craplab/spotifindme/BTSCanService.kt
+++ app/src/main/java/fi/craplab/spotifindme/BTSCanService.kt
@@ -14,9 +14,18 @@ import android.os.IBinder
 import android.util.Log
 import androidx.annotation.RequiresApi
 import org.greenrobot.eventbus.EventBus
+import android.content.BroadcastReceiver
+import android.content.IntentFilter
+import android.graphics.drawable.Icon
+import android.os.StrictMode
+import android.widget.Toast
+import java.lang.Exception
+
 
 class BTSCanService : Service() {
+    private var bcReceiver: BTSCanService.StopServerBroadcast? = null
     private var foundDevice: Any? = null
+
     @Suppress("PrivatePropertyName")
     private val Any.TAG: String
         get() {
@@ -25,20 +34,23 @@ class BTSCanService : Service() {
         }
 
 
-
     override fun onBind(intent: Intent): IBinder {
         Log.d(TAG, "service onBind")
         TODO("Return the communication channel to the service.")
     }
 
     private val bleScanner by lazy {
-        bluetoothAdapter.bluetoothLeScanner
+        bluetoothAdapter?.bluetoothLeScanner
     }
 
     // From the previous section:
-    private val bluetoothAdapter: BluetoothAdapter by lazy {
-        val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
-        bluetoothManager.adapter
+    private val bluetoothAdapter: BluetoothAdapter? by lazy {
+        try {
+            val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+            bluetoothManager.adapter
+        } catch (e: NullPointerException) {
+            null
+        }
     }
     private val scanSettings = ScanSettings.Builder()
         .setScanMode(ScanSettings.SCAN_MODE_LOW_POWER)
@@ -68,6 +80,7 @@ class BTSCanService : Service() {
                 }
             }
         }
+
         override fun onScanFailed(errorCode: Int) {
             Log.i(TAG, "Scan error: $errorCode")
             super.onScanFailed(errorCode)
@@ -85,16 +98,54 @@ class BTSCanService : Service() {
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate() {
         super.onCreate()
-        (getSystemService(NOTIFICATION_SERVICE) as NotificationManager).createNotificationChannel(
-            NotificationChannel("channel1", "spotifind notifications", NotificationManager.IMPORTANCE_DEFAULT)
-        )
-        startForeground(1,
-            Notification.Builder(this, "foo")
-                .setContentTitle("title")
-                .setContentText("context")
-                .setChannelId("channel1")
-                .setTicker("ticker_text")
-            .build())
+        StrictMode.enableDefaults()
+        Log.d(TAG, "${Build.VERSION.SDK_INT} >= ${Build.VERSION_CODES.O}")
+//        (getSystemService(NOTIFICATION_SERVICE) as NotificationManager).createNotificationChannel(
+//            NotificationChannel("channel1", "spotifind notifications", NotificationManager.IMPORTANCE_DEFAULT)
+//        )
+        val nfnChannel = NotificationChannel("channel1", "spotifind notifications",
+            NotificationManager.IMPORTANCE_LOW)
+        nfnChannel.setSound(null, null)
+        getSystemService(NotificationManager::class.java).createNotificationChannel(nfnChannel)
+        try {
+            startForeground(
+                1,
+                Notification.Builder(this, "channel1")
+                    .setContentTitle("SpotifindMe player")
+                    .setContentText("")
+//                    .setChannelId("channel1")
+                    .setSmallIcon(
+                        Icon.createWithResource(
+                            applicationContext,
+                            R.drawable.nfn_icon
+                        )
+                    ) //absolutely must have the above apparently...
+                    .setTicker("ticker_text")
+//                    .setPriority(Notification.PRIORITY_MAX)
+                    .addAction(
+                        Notification.Action.Builder(
+                            Icon.createWithResource(applicationContext, R.drawable.nfn_icon),
+                            "Exit",
+                            PendingIntent.getBroadcast(
+                                this, 1,
+//                                Intent(this, StopServerBroadcast::class.java)
+                                Intent("foo-ignored")
+//                                    .setAction("foo-ignored")
+                                    .putExtra(
+                                    "action",
+                                    "actionName"
+                                )
+                                ,PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+                            )
+                        ).build()
+                    ).setOngoing(true)
+                    .build()
+            )
+        } catch (e: Exception) {
+            Log.w(TAG, e)
+
+        }
+
 //        TODO add exit button
         Log.d(TAG, "service start")
         //bleScanner.startScan(null, scanSettings, scanCallback)
@@ -103,20 +154,34 @@ class BTSCanService : Service() {
     override fun onDestroy() {
         super.onDestroy()
         Log.d(TAG, "service destroy")
+        this.unregisterReceiver(this.bcReceiver)
     }
+
     private val filters = arrayListOf<ScanFilter>()
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         super.onStartCommand(intent, flags, startId)
         Log.d(TAG, "service onStartCommand")
+        this.bcReceiver = StopServerBroadcast()
+        this.registerReceiver(this.bcReceiver, IntentFilter("foo-ignored"))
 //        TODO Add filter - no filter gets killed on screen off
-//        For some reason, filter by name doesn't work, but MAC addr does.
+//        For some reason, filter by name doesn't work, but MAC address does.
 //        filters.add(ScanFilter.Builder().setDeviceName("BLAST").build())
         filters.add(ScanFilter.Builder().setDeviceAddress("EC:81:93:11:C4:41").build())
-        bleScanner.startScan(filters, scanSettings, scanCallback)
+        bleScanner?.startScan(filters, scanSettings, scanCallback)
         return START_STICKY
     }
 
-    data class DeviceMsg (val name: String, val found: Boolean)
+    data class DeviceMsg(val name: String, val found: Boolean)
 
+    inner class StopServerBroadcast : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            Toast.makeText(context, "received", Toast.LENGTH_SHORT).show()
+            val action = intent.getStringExtra("action")
+            Log.d(TAG, "Broadcast: action $action")
+            if (action == "actionName") {
+                this@BTSCanService.stopSelf()
+            }
+        }
+    }
 }
